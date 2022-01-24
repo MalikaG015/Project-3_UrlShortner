a2d2d45e37eea606a1e4d5925f97a1455a342840
MalikaG015 <malikagulati015@gmail.com>
2022-01-24 18:35:45 +0530

    Project-3_UrlShortner

---
+++ src/routes/route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+const urlController = require('../controllers/urlController');
+
+
+
+
+router.post('/url/shorten', urlController.generateUrl);
+router.get('/:urlCode', urlController.redirectToLongUrl);
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
